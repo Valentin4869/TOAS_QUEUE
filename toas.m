fd80ae590aebf824811a46478424fc213ec4e048
Valentin4869 <abc314@outlook.com>
2018-02-05 18:23:15 +0200

    Update toas.m

--- toas.m
+++ toas.m
@@ -3,13 +3,19 @@
 
 junk=[45;46;43;43;43;42;41;35;35;32;32;30;30;28;28;28;28];
 % j31  f1  f2
-q=[50;48;48];
+q=[50;...
+    48;48;46;45;44;43];
+
+%last entry: February 5th
 
 
 
-months={"February","March","April","May","June","July","August","September","October","November"};
-month="February";
+months={"January","February","March","April","May","June","July","August","September","October","November","December"};
+daysInMonth=[31; 28; 31; 30; 31; 30; 31; 31; 30; 31 ;30; 31];
+currentMonth="February";
 first_date=31;
+first_month=1;
+first_year=2018;
 
 plot(q)
 ylim([0,q(1)])
@@ -37,43 +43,46 @@ yC=yC((31-first_date+1+1:end));
 %remove feb
 if size(yC,2)>28
 yC=yC(28+1:end);
-month="March";
+currentMonth="March";
 end
 
 %remove March
 if size(yC,2)>31
 yC=yC(31:end);
-month="April";
+currentMonth="April";
 end
 
 %remove April
 if size(yC,2)>30
 yC=yC(30:end);
-month="May";
+currentMonth="May";
 end
 
 %remove May
 if size(yC,2)>31
 yC=yC(31:end);
-month="June";
+currentMonth="June";
 end
 
 %remove June
 if size(yC,2)>30
 yC=yC(30:end);
-month="July";
+currentMonth="July";
 end
 
 %remove July
 if size(yC,2)>31
 yC=yC(31:end);
-month="August";
+currentMonth="August";
 end
 
 %remove August
 if size(yC,2)>31
 yC=yC(31:end);
-month="September";
+currentMonth="September";
 end
 
-fprintf(strcat("\n",month," ",num2str(size(yC,2)),"\n"))
+fprintf(strcat("\n",currentMonth," ",num2str(size(yC,2)),"\n"))
+d=caldays(size(y(y>=0),2))- caldays(1)+datetime(2018,1,31) ;
+fprintf(strcat("MATLAB's: ",months{month(d)}," ",num2str(day(d)),", ",num2str(year(d)),"\n"));
+
